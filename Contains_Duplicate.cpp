2bed54d4514ee0952e66a3a64cc1838fcc01f50c
Sumaiya Islam <sumaiyaislam-14-2021711385@it.du.ac.bd>
2023-08-18 02:04:10 +0600

    Add files via upload

---
+++ Contains_Duplicate.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    bool containsDuplicate(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n-1; i++) 
+            if(nums[i]==nums[i+1]) return true;
+            
+        return false;
+        
+    }
+
