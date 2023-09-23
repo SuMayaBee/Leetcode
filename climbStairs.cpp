8048322c0b6cb823f2a099eba70897e1c30bbf8d
Sumaiya Islam <sumaiyaislam-14-2021711385@it.du.ac.bd>
2023-09-24 00:14:55 +0600

    Add files via upload

---
+++ climbStairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2) {
+            return n;
+        }
+
+        vector<int> a(n+1);
+        a[0]=0;
+        a[1]=1;
+        a[2]=2;
+
+        for(int i=3; i<=n; i++) {
+            a[i]= a[i-1] + a[i-2];
+        }
+
+        return a[n];
+
+        
+    }
+};
